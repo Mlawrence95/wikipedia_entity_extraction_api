e54cf3140ac8f6a4aa1aa1e64c4b8cd16d4ba9c4
Michael Lawrence <mikelawrence@berkeley.edu>
2020-02-23 00:04:03 -0800

    support saving output

--- make_request.py
+++ make_request.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 import requests
+import pandas as pd
 
+# format request
 payload = {
     'data': "https://en.wikipedia.org/wiki/Manhattan_Project"
 }
 
+# target location
 address = "http://127.0.0.1"
 port    = '5000'
-r = requests.get(f'{address}:{port}/wiki-entities', params=payload)
-print(r.text)
+
+
+if __name__ == "__main__":
+    # send request
+    r = requests.get(f'{address}:{port}/wiki-entities', params=payload)
+    output = r.json()['response_data']
+
+    save_path = "/Users/mikelawrence/Documents/GitHub/RESTful-wikient/example_response.csv"
+    # save data to spreadsheet
+    df = pd.DataFrame(output)
+    df.to_csv(save_path)
+
